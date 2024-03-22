14f865460f9ad4fac545b3c1a2258e7f74e42f78
eslamwaled150 <92927283+eslamwaled150@users.noreply.github.com>
2024-03-22 15:16:34 +0200

    update

---
+++ Car_Module/Car_Module/Debug/HAL/CAR_MOTOR/src/Car.d
@@ -0,0 +1,44 @@
+HAL/CAR_MOTOR/src/Car.d HAL/CAR_MOTOR/src/Car.o: \
+ ../HAL/CAR_MOTOR/src/Car.c ../HAL/CAR_MOTOR/inc/Car.h \
+ ../Library/STD_TYPES.h ../HAL/CAR_MOTOR/inc/Car_Confg.h \
+ ../MCAL/DIO/inc/DIO_interface.h ../Library/BIT_MATH.h ../Library/Clock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../MCAL/TMR1/inc/timer1.h ../Library/STD_Types.h \
+ ../MCAL/DIO/inc/DIO_config.h ../MCAL/TMR1/inc/timer1.h
+
+../HAL/CAR_MOTOR/inc/Car.h:
+
+../Library/STD_TYPES.h:
+
+../HAL/CAR_MOTOR/inc/Car_Confg.h:
+
+../MCAL/DIO/inc/DIO_interface.h:
+
+../Library/BIT_MATH.h:
+
+../Library/Clock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../MCAL/TMR1/inc/timer1.h:
+
+../Library/STD_Types.h:
+
+../MCAL/DIO/inc/DIO_config.h:
+
+../MCAL/TMR1/inc/timer1.h:
